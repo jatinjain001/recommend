ab84f4e8ebb354fc46be16c8ac4786d0ccb0247f
jatinjain001 <jatinjain20022001@gmail.com>
2021-11-04 01:52:25 +0530

    camera integrated

--- music/urls.py
+++ music/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('home',views.home),
+    path('camera', views.index,name='index'),
+     
+    path('video_feed', views.video_feed, name='video_feed'),#access the laptop camera
+    path('webcam_feed', views.webcam_feed, name='webcam_feed'),#access the mobile phone camera
 ]
--- music/views.py
+++ music/views.py
@@ -1,9 +1,36 @@
 from django.shortcuts import render
 import cv2
+from django.http import StreamingHttpResponse
+from . import camera
 # Create your views here.
 
 
 def home(request):
     return render(request,"index.html")
+
+def index(request):
+    return render(request, 'home.html')
+
+#Every time you call the phone and laptop camera method gets frame
+#More info found in camera.py
+def gen(camera):
+	while True:
+		frame = camera.get_frame()
+		yield (b'--frame\r\n'
+				b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+#Method for laptop camera
+def video_feed(request):
+	return StreamingHttpResponse(gen(camera.VideoCamera()),
+                    #video type
+					content_type='multipart/x-mixed-replace; boundary=frame')
+
+#Method for phone camera
+def webcam_feed(request):
+	return StreamingHttpResponse(gen(camera.IPWebCam()),
+                    #video type
+					content_type='multipart/x-mixed-replace; boundary=frame')
+
+
 
 
